9b4d64973d711bb8f35052a2ed380ab1b03e1606
Miguel Padilla <mpadi21@mit.edu>
2025-11-26 17:07:39 -0600

    Add modules for storage and DOM-manipulation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import './styles.css';
+import './styles.css'
+
+import TodoItemManager, { TodoItem, Priority } from './todo-item'
+import TodoListManager, { TodoList } from './todo-list'
+import StorageManager from './storage'
+import domManager from './dom'
 
 if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
+    console.log('Looks like we are in development mode!')
 }
