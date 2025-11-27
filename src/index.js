7b79bca9170abd2f45565135e4ed30870a3088cc
Miguel Padilla <mpadi21@mit.edu>
2025-11-26 23:50:19 -0600

    Add functionality to new list button to add new lists

--- src/index.js
+++ src/index.js
@@ -8,3 +8,25 @@ import domManager from './dom'
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!')
 }
+
+document.getElementById('new-list-button').addEventListener('click', () => {
+    const newListNameInput = document.getElementById('list-name')
+    const newListName = newListNameInput.value
+
+    if (newListName.length === 0) {
+        console.log('A new list must have a name of non-zero length')
+        return
+    }
+
+    console.log(`New list with name ${newListName}`)
+
+    // Add new list to manager
+    TodoListManager.createTodoList(newListName)
+
+    // Re-render lists with new list
+    const lists = TodoListManager.getTodoLists()
+    domManager.renderLists(lists)
+
+    // Reset input field
+    newListNameInput.value = ''
+})
