0b284d58fff7bcb74be0336d6f068c79a37a0d97
Miguel Padilla <mpadi21@mit.edu>
2025-11-26 23:49:21 -0600

    Add structure to dom.js for rendering items and lists

--- src/dom.js
+++ src/dom.js
@@ -2,5 +2,44 @@ import { TodoItem, Priority } from './todo-item'
 import { TodoList } from './todo-list'
 
 export default (() => {
-    
+    const listsContainer = document.querySelector('.lists')
+    const todosContainer = document.querySelector('.todos')
+
+    function resetTodos() {
+        while (todosContainer.firstChild) {
+            todosContainer.removeChild(todosContainer.firstChild)
+        }
+    }
+
+    function resetLists() {
+        while (listsContainer.firstChild) {
+            listsContainer.removeChild(listsContainer.firstChild)
+        }
+    }
+
+    function renderTodos(todos) {
+        resetTodos()
+
+        for (const todo of todos) {
+            if (!(todo instanceof TodoItem)) {
+                throw new Error(`A todo in todos given to renderTodos is not instance of TodoItem: ${todo}`)
+            }
+
+            // Add here
+        }
+    }
+
+    function renderLists(lists) {
+        resetLists()
+
+        for (const list of lists) {
+            if (!(list instanceof TodoList)) {
+                throw new Error(`A list in lists given to renderLists is not instance of TodoList: ${list}`)
+            }
+
+            // Add here
+        }
+    }
+
+    return { renderTodos, renderLists }
 })()
