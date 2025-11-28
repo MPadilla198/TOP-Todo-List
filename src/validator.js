3ec77fc0506eb24450525246bc0ba3c215272c4e
Miguel Padilla <mpadi21@mit.edu>
2025-11-28 00:02:09 -0600

    Add method in validator to validate new Todo Item

--- src/validator.js
+++ src/validator.js
@@ -8,5 +8,19 @@ export default (() => {
         return true
     }
 
-    return { isNewListNameValid }
+    function isNewTodoValid(todo) {
+        if (todo.title.length === 0) {
+            console.log('A new todo must have a title of non-zero length')
+            return false
+        }
+
+        if (todo.dueDate.length === 0) {
+            console.log('A new todo must have a due date')
+            return false
+        }
+
+        return true
+    }
+
+    return { isNewListNameValid, isNewTodoValid }
 })()
